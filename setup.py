562ca21154583c4c497dfce1a6fd7f5d3304fec9
rloizzo68 <ryanloizzo68@gmail.com>
2020-10-09 11:47:51 -0500

    create egg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
 	name = 'snscrape',
 	description = 'A social networking service scraper',
 	author = 'JustAnotherArchivist',
-	url = 'https://github.com/JustAnotherArchivist/snscrape',
+	url = 'https://github.com/steveodb/snscrape',
 	classifiers = [
 		'Development Status :: 4 - Beta',
 		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
